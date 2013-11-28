3713c6b294018ed9736c6b8c2983adee2c2950dc
kenu <kenu.heo@gmail.com>
2013-11-28 13:01:41 +0900

    검색어 공백처리

--- src/kr/kenuheo/searchimage/CustomizedListView.java
+++ src/kr/kenuheo/searchimage/CustomizedListView.java
@@ -1,4 +1,7 @@
 package kr.kenuheo.searchimage;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,7 +17,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
- 
+
 public class CustomizedListView extends Activity {
     // All static variables
     static String URL = "http://openapi.naver.com/search?key=352aa64aec19751e787bcfc612862f70&query=%EC%A0%9C%EC%A3%BC%EB%8F%84&target=image&start=1&display=10";
@@ -25,41 +28,47 @@ public class CustomizedListView extends Activity {
     static final String KEY_ARTIST = "sizewidth";
     static final String KEY_DURATION = "sizeheight";
     static final String KEY_THUMB_URL = "thumbnail";
- 
+
     ListView list;
     LazyAdapter adapter;
     EditText editText;
- 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText)findViewById(R.id.query);
-        
-        Button searchButton = (Button)findViewById(R.id.searchBtn);
+        editText = (EditText) findViewById(R.id.query);
+
+        Button searchButton = (Button) findViewById(R.id.searchBtn);
         searchButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-		        String query = editText.getText().toString();				
-		        URL = "http://openapi.naver.com/search?" +
-		        		"key=352aa64aec19751e787bcfc612862f70" +
-		        		"&target=image&start=1&display=10&query=" + query;
-		        fillList();
-			}
-		});
- 
+
+            @Override
+            public void onClick(View v) {
+                String query = editText.getText().toString();
+                try {
+                    query = URLEncoder.encode(query, "utf-8").replaceAll("\\+",
+                            "%20");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                URL = "http://openapi.naver.com/search?"
+                        + "key=352aa64aec19751e787bcfc612862f70"
+                        + "&target=image&start=1&display=10&query=" + query;
+                fillList();
+            }
+        });
+
         fillList();
     }
 
-	public void fillList() {
-		ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
- 
+    public void fillList() {
+        ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+
         XMLParser parser = new XMLParser();
-        
+
         String xml = parser.getXmlFromUrl(URL); // getting XML from URL
         Document doc = parser.getDomElement(xml); // getting DOM element
- 
+
         NodeList nl = doc.getElementsByTagName(KEY_SONG);
         // looping through all song nodes <song>
         for (int i = 0; i < nl.getLength(); i++) {
@@ -72,25 +81,25 @@ public class CustomizedListView extends Activity {
             map.put(KEY_ARTIST, parser.getValue(e, KEY_ARTIST));
             map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
             map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
- 
+
             // adding HashList to ArrayList
             songsList.add(map);
         }
- 
-        list=(ListView)findViewById(R.id.list);
- 
+
+        list = (ListView) findViewById(R.id.list);
+
         // Getting adapter by passing xml data ArrayList
-        adapter=new LazyAdapter(this, songsList);
+        adapter = new LazyAdapter(this, songsList);
         list.setAdapter(adapter);
- 
+
         // Click event for single list row
         list.setOnItemClickListener(new OnItemClickListener() {
- 
+
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                     int position, long id) {
- 
+
             }
         });
-	}
+    }
 }
